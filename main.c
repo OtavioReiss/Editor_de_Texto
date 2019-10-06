46010ef85fd8436f93167de5db929721eef3b42d
Otavio Reis <otavio.reis366@gmail.com>
2019-10-05 23:14:02 -0300

    Adicionado Menu a interface

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <Windows.h>
 #include <stdio.h>
 
+/* DECLARAÇÃO DAS FUNÇÕES */
 LRESULT CALLBACK WProcedure(HWND, UINT, WPARAM, LPARAM);
+void addMenus(HWND);
+HMENU hMenu;
 
 /* Função pricipal da Biblioteca Windows.h */
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
@@ -33,10 +36,28 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdsho
     return 0;
 }
 
+
+/* FUNÇÃO DE CALLBACK PARA CONTROLE DA JANELA*/
+
 LRESULT CALLBACK WProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 {
     switch (msg)
     {
+        case WM_COMMAND: // CASO QUE CONTROLAR AS AÇÕES DO MENU
+            switch (wp)
+            {
+                case 1:
+                    MessageBeep(MB_OK);
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+            break;
+        case WM_CREATE:
+            addMenus(hWnd);
+            break;
          case WM_DESTROY:
             PostQuitMessage(0);
             break;
@@ -44,5 +65,16 @@ LRESULT CALLBACK WProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
             return DefWindowProcW(hWnd, msg, wp ,lp);
     }
 
+}
+/* BARRA DE MENU DO PROGRAMA */
+void addMenus(HWND hWnd)
+{
+    hMenu = CreateMenu();
+
+    AppendMenu(hMenu, MF_STRING, 1,"Salvar");
+    AppendMenu(hMenu, MF_STRING, 2,"Carregar");
+    AppendMenu(hMenu, MF_STRING, 3,"Sobre");
+
+    SetMenu(hWnd, hMenu);
 
 }
