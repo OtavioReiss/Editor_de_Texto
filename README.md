06d39f88715909e32c12d5b216e6c0070139abc2
Otavio Reis <otavio.reis366@gmail.com>
2019-10-06 15:24:46 -0300

    Added text input box

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Editor_de_Texto
-Trabalho de Técnicas de Programação A.
-UEL 2º SEMESTRE 2019.
+
+Editor de Texo é um trabalho entregue a disciplina de Técnicas de Programação A, 
+para o 2º semestre do Curso de Ciência da computação da Universidade Estadual de Londrina.
+
+- Linguagem: C.
+
+- Biblioteca: Windows.h
+
+- Feito por:
+    Otavio B. dos Reis (otavio.reis366@gmail.com).
+
+Doscente: Jandira Guenka Palma.
--- main.c
+++ main.c
@@ -9,9 +9,11 @@
 /* DECLARAÇÃO DAS FUNÇÕES */
 LRESULT CALLBACK WProcedure(HWND, UINT, WPARAM, LPARAM);
 void addMenus(HWND);
-HMENU hMenu/*()*/;
+HMENU hMenu;//();
+void AddControls(HWND);
+HWND TextRecive;
 
-/* Função pricipal da Biblioteca Windows.h */
+/* Função pricipal da Biblioteca Windows.h, Equivalente a função Main() */
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
 {
     
@@ -59,25 +61,28 @@ LRESULT CALLBACK WProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
                     break;
             }
             break;
-        case WM_CREATE:
+
+        case WM_CREATE://QUANDO A JANELA É CRIADA AS FUNÇÕES ABAIXOS SÃO CHAMADAS PELA PRIMEIRA VEZ
             addMenus(hWnd);
+            AddControls(hWnd);
             break;
-         case WM_DESTROY:
+
+         case WM_DESTROY: //QUANDO A JANELA É FINALIZADA AS FUNÇÕES ABAIXOS SÃO CHAMADAS
             PostQuitMessage(0);
             break;
         default:
             return DefWindowProcW(hWnd, msg, wp ,lp);
     }
-
 }
+
 /* BARRA DE MENU DO PROGRAMA */
 void addMenus(HWND hWnd)
 {
     hMenu = CreateMenu();
     HMENU hFileMenu = CreateMenu();
     HMENU hSubMenu = CreateMenu();
-    /* SUB MENU --ARQUIVO-- */
 
+    /* SUB MENU --ARQUIVO-- */
     AppendMenu(hFileMenu, MF_STRING, ARQ_MENU_NOVO, "Novo");
     AppendMenu(hFileMenu, MF_STRING, ARQ_MENU_SALV, "Salvar");
     AppendMenu(hFileMenu, MF_STRING, ARQ_MENU_CARR, "Carregar");
@@ -89,5 +94,12 @@ void addMenus(HWND hWnd)
     AppendMenu(hMenu, MF_STRING, 2,"Sobre");
 
     SetMenu(hWnd, hMenu);
+}
 
+//FUNÇÃO QUE CONTROLA AS APLICAÇÕES
+void AddControls(HWND hWnd)
+{
+    //CreateWindowW(L"static", L"Enter the text here: ", WS_VISIBLE | WS_CHILD , 0, 0, 1920, 1080, hWnd, NULL, NULL, NULL);
+    /*CRIA O CAMPO DE ESCRITA*/
+    TextRecive = CreateWindowW(L"edit", L"",  WS_VISIBLE | WS_CHILD  | ES_MULTILINE | ES_LEFT | EM_GETSEL  | ES_AUTOHSCROLL | ES_AUTOVSCROLL | EM_UNDO, 0, 0, 500, 500, hWnd, NULL, NULL, NULL);
 }
